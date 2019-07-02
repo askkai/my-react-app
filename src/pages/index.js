440bb1297bf31b62e3cc2a70bb942a38bc38c217
cary03 <akai9303@gmail.com>
2019-07-03 00:03:06 +0800

    index layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,17 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>How are you?</h1>
-    <p>I'm fine, thank u</p>
-    <p>And u?</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Watch the video</Link>
-  </Layout>
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>How are you?</h1>
+        <p>I'm fine, thank u</p>
+        <p>And u?</p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div> 
+
+  </div>
 )
 
 export default IndexPage
