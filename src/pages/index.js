cfc8bf9248a69e8074fdb5206e3a72bad5e3f057
cary03 <akai9303@gmail.com>
2019-07-02 16:04:16 +0800

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>How are you?</h1>
+    <p>I'm fine, thank u</p>
+    <p>And u?</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch the video</Link>
   </Layout>
 )
 
