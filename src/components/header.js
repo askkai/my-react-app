85c8c3ddb20f500fa2f1903184756c397590e344
cary03 <akai9303@gmail.com>
2019-07-25 00:01:45 +0800

    animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,26 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div>
-    Header
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo-test1.svg')} width="30" /></Link>
+      <Link to="/article">Article</Link>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact me"><button>Contact me</button></Link>
+    </div>
   </div>
 )
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header";
 
 const IndexPage = () => (
   <div>
+    <Header />
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>How are you?</h1>
+        <h1>Nice to meet you</h1>
         <p>I'm fine, thank u</p>
         <p>And u?</p>
         <Link to="/page-2/">Watch the video</Link>
